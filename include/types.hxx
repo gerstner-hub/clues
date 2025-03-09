55af671da70a35b68c2ba7fc23ba85eb794e1218
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2025-03-09 22:40:19 +0100

    types.hxx: add KernelErrno

--- include/types.hxx
+++ include/types.hxx
@@ -21,4 +21,18 @@ enum class Word : elf_greg_t {
 	ZERO = 0
 };
 
+/// Errno values that can appear in tracing context.
+/**
+ * these errno values can appear for interrupted system calls and are not
+ * usually visible in user space, but can be observed when tracing a process.
+ * There is currently no user space header for these constants, they are found
+ * in the kernel header linux/errno.h.
+ **/
+enum class KernelErrno : int {
+	RESTART_SYS          = 512, /* transparent restart if SA_RESTART is set, otherwise EINTR return */
+	RESTART_NOINTR       = 513, /* always transparent restart */
+	RESTART_NOHAND       = 514, /* restart if no handler.. */
+	RESTART_RESTARTBLOCK = 516, /* restart by calling sys_restart_syscall */
+};
+
 } // end ns
