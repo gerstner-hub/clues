2313833bbfaad78ef4d0f293f4076beb832d50b7
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2025-03-21 19:09:57 +0200

    Arch.hxx: use libcosmos and change spelling of getRegisterName()
    - global free functions should be snake case.
    - use libcosmos features for architecture detection.

--- include/Arch.hxx
+++ include/Arch.hxx
@@ -3,8 +3,11 @@
 // C++
 #include <cstdlib>
 
+// cosmos
+#include <cosmos/compiler.hxx>
+
 // Linux
-#if defined(__x86_64__) || defined(__i386__)
+#ifdef COSMOS_X86
 // provides offsets into the ptrace register data structure
 // does only seem to exist on x86/64, on ARM it's simply 18 GP registers from
 // 0 to 18.
@@ -12,7 +15,7 @@
 // elf_gregset_t is based on the structure and the array is easier to access
 // for our purposes.
 #	include <sys/reg.h>
-#elif defined(__arm__)
+#elif defined(COSMOS_ARM)
 // for arm some R* constants are found in this header. The constants are
 // actually just names for the numbers 0 ... 15 but it is still somewhat
 // clearer to read this way.
@@ -26,35 +29,6 @@
 
 namespace clues {
 
-inline constexpr bool isx86_64() {
-#ifdef __x86_64__
-	return true;
-#else
-	return false;
-#endif
-}
-
-inline constexpr bool isx86() {
-#ifdef __i386__
-	return true;
-#else
-	return false;
-#endif
-}
-
-inline constexpr bool isi386() {
-	return isx86();
-}
-
-// this currently only means Arm EABI
-inline constexpr bool isArm() {
-#ifdef __arm__
-	return true;
-#else
-	return false;
-#endif
-}
-
 /*
  * orig_eax saves the original system call number during a system call,
  * because the actual eax is used to return the system call result.
@@ -64,7 +38,7 @@ inline constexpr bool isArm() {
  * The syscall ABI is documented in man(2) syscall
  */
 
-#ifdef __x86_64__
+#ifdef COSMOS_X86_64
 constexpr int SYSCALL_MAX_PARS = 6;
 constexpr int SYSCALL_NR_REG   = ORIG_RAX;
 constexpr int SYSCALL_RES_REG  = RAX;
@@ -74,7 +48,7 @@ constexpr int SYSCALL_PAR3_REG = RDX;
 constexpr int SYSCALL_PAR4_REG = R10;
 constexpr int SYSCALL_PAR5_REG = R8;
 constexpr int SYSCALL_PAR6_REG = R9;
-#elif defined(__i386__)
+#elif defined(COSMOS_I386)
 constexpr int SYSCALL_MAX_PARS = 6;
 constexpr int SYSCALL_NR_REG   = ORIG_EAX;
 constexpr int SYSCALL_RES_REG  = EAX;
@@ -84,7 +58,7 @@ constexpr int SYSCALL_PAR3_REG = EDX;
 constexpr int SYSCALL_PAR4_REG = ESI;
 constexpr int SYSCALL_PAR5_REG = EDI;
 constexpr int SYSCALL_PAR6_REG = EBP;
-#elif defined(__arm__)
+#elif defined(COSMOS_ARM)
 constexpr int SYSCALL_MAX_PARS = 7;
 constexpr int SYSCALL_NR_REG   = REG_R7;
 constexpr int SYSCALL_RES_REG  = REG_R0;
@@ -119,6 +93,6 @@ static constexpr int SYSCALL_PAR_REGISTER[] = {
 };
 
 /// Returns a human readable name for the given platform specific register number in a RegisterSet.
-const char* getRegisterName(const size_t nr);
+const char* get_register_name(const size_t nr);
 
 } // end ns
--- src/Arch.cxx
+++ src/Arch.cxx
@@ -2,82 +2,72 @@
 
 // Cosmos
 #include <cosmos/error/UsageError.hxx>
+#include <cosmos/formatting.hxx>
 
 // Clues
 #include <clues/Arch.hxx>
 
 namespace clues {
 
-const char* getRegisterName(const size_t number) {
+const char* get_register_name(const size_t number) {
+
+#ifdef COSMOS_X86_64
+	switch(number) {
+		case R15: return "R15";
+		case R14: return "R14";
+		case R13: return "R13";
+		case R12: return "R12";
+		case RBP: return "RBP";
+		case RBX: return "RBX";
+		case R11: return "R11";
+		case R10: return "R10";
+		case R9:  return "R9";
+		case R8:  return "R8";
+		case RAX: return "RAX";
+		case RCX: return "RCX";
+		case RDX: return "RDX";
+		case RSI: return "RSI";
+		case RDI: return "RDI";
+		case ORIG_RAX: return "ORIG_RAX";
+		case RIP: return "RIP";
+		case CS:  return "CS";
+		case EFLAGS: return "AGS";
+		case RSP: return "RSP";
+		case SS:  return "SS";
+		case FS_BASE: return "FS_BASE";
+		case GS_BASE: return "GS_BASE";
+		case DS: return "DS";
+		case ES: return "ES";
+		case FS: return "FS";
+		case GS: return "GS";
+	}
+#elif defined(COSMOS_I386)
 	switch (number) {
-	default: cosmos_throw (cosmos::UsageError("Invalid register number encountered")); return "";
-#ifdef __x86_64__
-	case R15: return "R15";
-	case R14: return "R14";
-	case R13: return "R13";
-	case R12: return "R12";
-	case RBP: return "RBP";
-	case RBX: return "RBX";
-	case R11: return "R11";
-	case R10: return "R10";
-	case R9: return "R9";
-	case R8: return "R8";
-	case RAX: return "RAX";
-	case RCX: return "RCX";
-	case RDX: return "RDX";
-	case RSI: return "RSI";
-	case RDI: return "RDI";
-	case ORIG_RAX: return "ORIG_RAX";
-	case RIP: return "RIP";
-	case CS: return "CS";
-	case EFLAGS: return "AGS";
-	case RSP: return "RSP";
-	case SS: return "SS";
-	case FS_BASE: return "FS_BASE";
-	case GS_BASE: return "GS_BASE";
-	case DS: return "DS";
-	case ES: return "ES";
-	case FS: return "FS";
-	case GS: return "GS";
-#elif defined(__i386__)
-	case EBX: return "EBX";
-	case ECX: return "ECX";
-	case EDX: return "EDX";
-	case ESI: return "ESI";
-	case EDI: return "EDI";
-	case EBP: return "EBP";
-	case EAX: return "EAX";
-	case DS: return "DS";
-	case ES: return "ES";
-	case FS: return "FS";
-	case GS: return "GS";
-	case ORIG_EAX: return "ORIG_EAX";
-	case EIP: return "EIP";
-	case CS: return "CS";
-	case EFL: return "EFL";
-	case UESP: return "UESP";
-	case SS: return "SS";
-#elif defined(__arm__)
-	case 0: return "r0";
-	case 1: return "r1";
-	case 2: return "r2";
-	case 3: return "r3";
-	case 4: return "r4";
-	case 5: return "r5";
-	case 6: return "r6";
-	case 7: return "r7";
-	case 8: return "r8";
-	case 9: return "r9";
-	case 10: return "r10";
-	case 11: return "r11";
-	case 12: return "r12";
-	case 13: return "r13";
-	case 14: return "r14";
-	case 15: return "r15";
-#else
-#	error "not yet supported architecture"
+		case EBX: return "EBX";
+		case ECX: return "ECX";
+		case EDX: return "EDX";
+		case ESI: return "ESI";
+		case EDI: return "EDI";
+		case EBP: return "EBP";
+		case EAX: return "EAX";
+		case DS:  return "DS";
+		case ES:  return "ES";
+		case FS:  return "FS";
+		case GS:  return "GS";
+		case ORIG_EAX: return "ORIG_EAX";
+		case EIP: return "EIP";
+		case CS:  return "CS";
+		case EFL: return "EFL";
+		case UESP: return "UESP";
+		case SS:  return "SS";
+	}
+#elif defined(COSMOS_ARM)
+	if (number >= 0 && number <= 15) {
+		return cosmos::sprintf("r%zd", number);
+	}
 #endif
-	} // end switch
+
+	cosmos_throw (cosmos::UsageError("Invalid register number encountered")); return "";
 }
 
 } // end ns
