750498b2012a91270062e5c34fd538e9c44ee5fe
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2025-07-18 20:48:29 +0200

    tests: add execve in non-main-thread test case

--- test/execve.cxx
+++ test/execve.cxx
@@ -1,5 +1,6 @@
 // C++
 #include <iostream>
+#include <vector>
 
 // Cosmos
 #include <cosmos/fs/File.hxx>
@@ -76,6 +77,10 @@ public:
 	auto& newExecutable() const {
 		return m_new_executable;
 	}
+
+	auto& attachedPIDs() const {
+		return m_attached_pids;
+	}
 protected:
 	void syscallEntry(clues::Tracee &,
 			const clues::SystemCall &call,
@@ -96,8 +101,9 @@ protected:
 		}
 	}
 
-	void attached(clues::Tracee&) override {
+	void attached(clues::Tracee &tracee) override {
 		m_seen_attached++;
+		m_attached_pids.push_back(tracee.pid());
 	}
 
 	void exited(clues::Tracee &, const cosmos::WaitStatus status, const State state) override {
@@ -130,6 +136,8 @@ protected:
 	cosmos::StringVector m_old_cmdline;
 	std::optional<cosmos::ProcessID> m_old_pid;
 	std::string m_new_executable;
+	// PIDs we've seen during attached() in order of appearance
+	std::vector<cosmos::ProcessID> m_attached_pids;
 };
 
 cosmos::ConditionMutex condition;
@@ -146,10 +154,17 @@ cosmos::pthread::ExitValue thread_entry_blocking(cosmos::pthread::ThreadArg) {
 	return cosmos::pthread::ExitValue{0};
 }
 
+cosmos::pthread::ExitValue thread_entry_exec(cosmos::pthread::ThreadArg) {
+	auto true_exe = cosmos::fs::which("true");
+	cosmos::proc::exec(*true_exe);
+	return cosmos::pthread::ExitValue{0};
+}
+
 class ExecveTest : public cosmos::TestBase {
 	void runTests() override {
 		testRegularExecve();
 		testMainThreadExecve();
+		testOtherThreadExecve();
 	}
 
 	void testRegularExecve() {
@@ -231,6 +246,40 @@ class ExecveTest : public cosmos::TestBase {
 				tracer.waitStatus()->status() == cosmos::ExitStatus::FAILURE);
 		RUN_STEP("execve-old-pid-is-not-there", tracer.oldPID() == std::nullopt);
 		RUN_STEP("new-executable-matches-false", tracer.newExecutable() == false_exe);
+		if (!onValgrind()) {
+			// when running on valgrind then there's a valgrind frontend instead, confusing things
+			RUN_STEP("old-executable-is-us", tracer.oldExecutable().find("execve") != std::string::npos);
+			RUN_STEP("old-cmdline-is-ours", tracer.oldCmdline() == m_argv);
+		}
+	}
+
+	void testOtherThreadExecve() {
+		START_TEST("non-main-thread execve (multithreaded)");
+
+		ExecveTracer tracer{clues::SystemCallNr::EXECVE};
+
+		TraceeCreator creator{[]() {
+				// create a random thread that runs execve
+				cosmos::PosixThread thread{
+					thread_entry_exec, cosmos::pthread::ThreadArg{0}};
+				// just block to avoid an exit of the main thread.
+				thread.join();
+			}, tracer};
+		creator.run(clues::FollowChildren{true});
+		RUN_STEP("seen-execve-entry", tracer.numExecEntrySeen() == 1);
+		RUN_STEP("seen-execve-exit", tracer.numExecExitSeen() == 1);
+		RUN_STEP("num-entry-matches-num-exit",
+				tracer.numExecEntrySeen() == tracer.numExecExitSeen());
+		RUN_STEP("seen-successful-execve", tracer.numSuccessfulExecSeen() == 1);
+		RUN_STEP("seen-attached-twice", tracer.numSeenAttached() == 2);
+		RUN_STEP("seen-exited", tracer.waitStatus() != std::nullopt);
+		RUN_STEP("num-exited-is-two", tracer.numSeenExits() == 2);
+		RUN_STEP("seen-lost-to-execve", tracer.numSeenLostToExecve() == 1);
+		RUN_STEP("wait-status-is-zero",
+				tracer.waitStatus()->exited() &&
+				tracer.waitStatus()->status() == cosmos::ExitStatus::SUCCESS);
+		RUN_STEP("execve-old-pid-matches-other-thread", tracer.oldPID() == tracer.attachedPIDs().back());
+		RUN_STEP("new-executable-matches-true", tracer.newExecutable().find("true") != std::string::npos);
 		if (!onValgrind()) {
 			// when running on valgrind then there's a valgrind frontend instead, confusing things
 			RUN_STEP("old-executable-is-us", tracer.oldExecutable().find("execve") != std::string::npos);
