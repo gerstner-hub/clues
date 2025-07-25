48274839a225da75a55ff218dd5e299d5daf21c4
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2025-07-25 19:49:16 +0200

    test: move main-thread-exec into helpers and use libcosmos API

---
+++ test/helpers/main-thread-exec.cxx
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cosmos/main.hxx>
+#include <cosmos/formatting.hxx>
+#include <cosmos/proc/process.hxx>
+#include <cosmos/thread/PosixThread.hxx>
+#include <cosmos/time/time.hxx>
+#include <cosmos/fs/filesystem.hxx>
+
+/*
+ * this helper performs an execve() while another thread is still alive
+ */
+
+class MainThreadExec :
+	public cosmos::MainContainerArgs {
+public:
+	cosmos::ExitStatus main(const std::string_view argv0, const cosmos::StringViewVector &args) override;
+};
+
+cosmos::pthread::ExitValue thread_entry(cosmos::pthread::ThreadArg) {
+	std::cout << "other thread pid is " << cosmos::thread::get_tid() << std::endl;
+	cosmos::time::sleep(std::chrono::milliseconds(1000 * 1000));
+	return cosmos::pthread::ExitValue{0};
+}
+
+cosmos::ExitStatus MainThreadExec::main(const std::string_view,
+		const cosmos::StringViewVector &args) {
+	std::cout << "main thread pid is " << cosmos::proc::get_own_pid() << std::endl;
+	cosmos::PosixThread thread{&thread_entry, cosmos::pthread::ThreadArg{0}};
+
+	if (!args.empty()) {
+		std::cerr << "press enter to execve()\n";
+		bool ready;
+		std::cin >> ready;
+	} else {
+		// give the other thread some time to output its message
+		cosmos::time::sleep(std::chrono::milliseconds(200));
+	}
+
+	auto true_exe = cosmos::fs::which("true");
+
+	if (!true_exe) {
+		std::cerr << "couldn't find true program?!\n";
+		cosmos::proc::exit(cosmos::ExitStatus{2});
+	}
+
+	cosmos::proc::exec(*true_exe, cosmos::CStringVector{true_exe->c_str(), nullptr});
+	std::cerr << "a life post-exec?!\n";
+	return cosmos::ExitStatus::FAILURE;
+}
+
+int main(int argc, const char **argv) {
+	return cosmos::main<MainThreadExec>(argc, argv);
+}
