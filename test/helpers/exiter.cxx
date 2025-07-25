ba162fc6a1ac3a1add4667d50af12e1f4296961b
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2025-07-25 20:12:00 +0200

    test: exiter.cxx: use libcosmos API

--- test/helpers/exiter.cxx
+++ test/helpers/exiter.cxx
@@ -1,15 +1,32 @@
 #include <cstdlib>
+#include <iostream>
 #include <cosmos/proc/process.hxx>
+#include <cosmos/main.hxx>
 
-int main(int argc, const char **argv) {
-	if (argc < 2) {
-		return 0;
+/*
+ * this program simply returns the exit status given on the command line
+ */
+
+class Exiter :
+	public cosmos::MainContainerArgs {
+public:
+	cosmos::ExitStatus main(const std::string_view argv0, const cosmos::StringViewVector &args) override;
+};
+
+cosmos::ExitStatus Exiter::main(const std::string_view, const cosmos::StringViewVector &args) {
+	if (args.empty()) {
+		return cosmos::ExitStatus::SUCCESS;
 	}
 
 	try {
-		auto exit_status = std::strtoul(argv[1], nullptr, 10);
-		return static_cast<int>(exit_status);
-	} catch (const std::exception &) {
-		return 1;
+		auto exit_status = std::stoi(args[0].data());
+		return cosmos::ExitStatus{exit_status};
+	} catch (const std::exception &ex) {
+		std::cerr << ex.what() << std::endl;
+		return cosmos::ExitStatus::FAILURE;
 	}
+}
+
+int main(int argc, const char **argv) {
+	return cosmos::main<Exiter>(argc, argv);
 }
