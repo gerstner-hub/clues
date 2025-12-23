159280ec948c2dd0c2356eaf1365e1bf07c448d9
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2025-12-23 17:45:03 +0100

    ArchCodeParameter: always compile str() function, also support X86
    Other architectures report linker errors when the `str()` implementation
    is not present. Also there are a few constants supported on both X86 and
    X86_64.

--- src/items/prctl.cxx
+++ src/items/prctl.cxx
@@ -2,7 +2,7 @@
 #include <cosmos/compiler.hxx>
 
 // Linux
-#ifdef COSMOS_X86_64
+#ifdef COSMOS_X86
 #	include <asm/prctl.h> // arch_prctl constants
 #	include <sys/prctl.h>
 #endif
@@ -13,16 +13,20 @@
 
 namespace clues::item {
 
-#ifdef COSMOS_X86_64
 std::string ArchCodeParameter::str() const {
 	switch (valueAs<int>()) {
+#ifdef COSMOS_X86
+		CASE_ENUM_TO_STR(ARCH_SET_CPUID);
+		CASE_ENUM_TO_STR(ARCH_GET_CPUID);
+#ifdef COSMOS_X86_64
 		CASE_ENUM_TO_STR(ARCH_SET_FS);
 		CASE_ENUM_TO_STR(ARCH_GET_FS);
 		CASE_ENUM_TO_STR(ARCH_SET_GS);
 		CASE_ENUM_TO_STR(ARCH_GET_GS);
+#endif // COSMOS_X86_64
+#endif // COSMOS_X86
 		default: return "unknown";
 	}
 }
-#endif // COSMOS_X86_64
 
 } // end ns
