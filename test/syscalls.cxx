4aab2e8af429a374396e18574e0c3e290499aaf0
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2026-03-10 19:10:04 +0100

    unit tests: add per-syscall unit test framework
    Each system call should be accompanied by some unit tests. This adds a
    simple framework for doing this. For a start, the AccessSystemCall is
    covered.

---
+++ test/syscalls.cxx
@@ -0,0 +1,151 @@
+// C++
+#include <iostream>
+
+// clues
+#include <clues/Tracee.hxx>
+#include <clues/syscalls/fs.hxx>
+
+// Test
+#include "TestBase.hxx"
+#include "TraceeCreator.hxx"
+
+/*
+ * This is a collective unit test for every single system call.
+ *
+ * This is a test purely on library API level. We're setting up a child
+ * process which executes a lambda function which executes one or more target
+ * system call variants, for which system call enter and exit callbacks are
+ * expected, again running lambda functions to verify the observed data.
+ */
+
+using clues::SystemCall;
+using TraceVerifyCB = std::function<bool (const SystemCall &)>;
+using SyscallInvoker = std::function<void (void)>;
+
+class SyscallTracer : public clues::EventConsumer {
+public:
+	explicit SyscallTracer(TraceVerifyCB enter_verify,
+			TraceVerifyCB exit_verify) :
+				m_enter_verify{enter_verify},
+				m_exit_verify{exit_verify} {
+
+	}
+
+	void syscallEntry(clues::Tracee &,
+			const SystemCall &call,
+			const StatusFlags) override {
+		if (m_ran_cbs) {
+			return;
+		} else if (!m_seen_initial_read) {
+			return;
+		}
+
+		m_entry_good = m_enter_verify(call);
+	}
+
+	void syscallExit(clues::Tracee &,
+			const SystemCall &call,
+			const StatusFlags) override {
+		if (m_ran_cbs) {
+			return;
+		} else if (!m_seen_initial_read) {
+		       if (call.callNr() == clues::SystemCallNr::READ) {
+				/* Tracee Creator performs a read() system
+				 * call for synchronization which we need to
+				 * skip */
+				m_seen_initial_read = true;
+			}
+			return;
+		}
+
+		m_exit_good = m_exit_verify(call);
+		m_ran_cbs = true;
+	}
+
+	void setCreator(TraceeCreator<SyscallInvoker> &creator) {
+		/*
+		 * we need this to synchronize exactly to the eventfd read()
+		 * in the child in attached().
+		 */
+		m_creator = &creator;
+	}
+
+	void attached(clues::Tracee &) override {
+		m_creator->signalChild();
+	}
+
+	bool entryGood() const {
+		return m_entry_good;
+	}
+
+	bool exitGood() const {
+		return m_exit_good;
+	}
+
+protected:
+
+	TraceVerifyCB m_enter_verify;
+	TraceVerifyCB m_exit_verify;
+	bool m_entry_good = false;
+	bool m_exit_good = false;
+	bool m_seen_initial_read = false;
+	bool m_ran_cbs = false;
+	TraceeCreator<SyscallInvoker> *m_creator = nullptr;
+};
+
+template <typename SC>
+const SC& upcast(const SystemCall &sc) {
+	return dynamic_cast<const SC&>(sc);
+}
+
+class SyscallTest :
+	public cosmos::TestBase {
+
+	void runTests() override;
+
+	void runTrace(
+			const std::string_view name,
+			SyscallInvoker invoker,
+			TraceVerifyCB enter_verify,
+			TraceVerifyCB exit_verify) {
+		START_TEST(cosmos::sprintf("testing system call %s", &name[0]));
+		SyscallTracer tracer{enter_verify, exit_verify};
+
+		TraceeCreator creator{std::move(invoker), tracer};
+		tracer.setCreator(creator);
+		try {
+			creator.run(clues::FollowChildren{false},
+					/*explicit_signal=*/true);
+		} catch (const std::exception &ex) {
+			std::cerr << "test failed: " << ex.what() << std::endl;
+		}
+		RUN_STEP("system call entry good", tracer.entryGood());
+		RUN_STEP("system call exit good", tracer.exitGood());
+	}
+};
+
+void SyscallTest::runTests() {
+	runTrace("access()",
+		[]() {
+			access("/etc/", R_OK|X_OK);
+		},
+		[](const SystemCall &sc) {
+			auto &access_sc = upcast<clues::AccessSystemCall>(sc);
+			if (access_sc.path.data() != "/etc/")
+				return false;
+			using cosmos::fs::AccessCheck;
+			using cosmos::fs::AccessChecks;
+			const AccessChecks checks{AccessCheck::READ_OK, AccessCheck::EXEC_OK};
+			if ((*access_sc.mode.checks()) != checks) {
+				return false;
+			}
+			return true;
+		}, [](const SystemCall &sc) {
+			return !sc.hasErrorCode();
+		});
+}
+
+int main(const int argc, const char **argv) {
+	SyscallTest test;
+	return test.run(argc, argv);
+}
