286a46caf8d98b8c20f0b0d2d07a2843a9ebaaf0
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2026-03-29 14:26:07 +0200

    sigaction sample: output size of sigset and sigaction as visible from user space

--- test/samples/sigaction.cxx
+++ test/samples/sigaction.cxx
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <signal.h>
 
 #include <cosmos/memory.hxx>
@@ -11,10 +13,13 @@ int main() {
 
 	cosmos::zero_object(act);
 	cosmos::zero_object(oldact);
-	
+
 	act.sa_handler = sighandler;
 	sigaddset(&act.sa_mask, SIGUSR1);
 	act.sa_flags = SA_RESTART|SA_RESETHAND;
 
 	::sigaction(SIGCHLD, &act, &oldact);
+
+	std::cout << "sigaction: " << sizeof(struct sigaction) << " bytes\n";
+	std::cout << "sigset_t: " << sizeof(sigset_t) << " bytes\n";
 }
