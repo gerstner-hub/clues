3332e18429634c26eaa1461edb60f26703fd0e6a
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2026-05-30 16:46:53 +0200

    samples: add readv() call

--- test/samples/read.cxx
+++ test/samples/read.cxx
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/uio.h>
 
 int main() {
 	int fd = open("/etc/fstab", O_RDONLY);
@@ -9,6 +10,20 @@ int main() {
 	}
 
 	if (pread(fd, buffer, sizeof(buffer), 20) < 0) {
+
+	}
+
+	char buffer2[1024];
+
+	struct iovec vec[2];
+	vec[0].iov_base = (void*)buffer;
+	vec[0].iov_len = 24;
+	vec[1].iov_base = (void*)buffer2;
+	vec[1].iov_len = 16;
+
+	lseek(fd, SEEK_SET, 0);
+
+	if (readv(fd, vec, 2) < 0 ) {
 
 	}
 }
