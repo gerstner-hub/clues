333aba10bc3966eb32119a698e26a8ce9d0b7660
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2026-06-14 19:17:39 +0100

    test/syscalls.cxx: add first batch of prctl() tests

--- test/syscalls.cxx
+++ test/syscalls.cxx
@@ -2663,6 +2663,123 @@ const auto TESTS = std::array{
 				syscall32(SyscallNr32::RSEQ, rs, RS_SIZE, 0, 0x12345678);
 			})
 		}
+	}, TestSpec{SystemCallNr::PRCTL, []() {
+			prctl(PR_SET_DUMPABLE, 1);
+			prctl(PR_GET_DUMPABLE);
+		}, ENTRY_VERIFY_CB(PrCtlSystemCall, {
+			VERIFY(sc.op.operation() ==
+					clues::item::ProcessOp::GET_DUMPABLE);
+			VERIFY(!sc.bool_setting);
+			VERIFY(!sc.res);
+			VERIFY(sc.bool_res.has_value());
+		}), EXIT_VERIFY_CB(PrCtlSystemCall, {
+			VERIFY(sc.hasResultValue());
+			VERIFY(sc.bool_res->value() == true);
+		}), IgnoreCalls{1}, {
+			I386_CROSS_ABI(IgnoreCalls{1}, []() {
+				prctl(PR_SET_DUMPABLE, 1);
+				syscall32(SyscallNr32::PRCTL, PR_GET_DUMPABLE);
+			})
+		}, "GET_DUMPABLE"
+	}, TestSpec{SystemCallNr::PRCTL, []() {
+			prctl(PR_SET_DUMPABLE, 1);
+		}, ENTRY_VERIFY_CB(PrCtlSystemCall, {
+			VERIFY(sc.op.operation() ==
+					clues::item::ProcessOp::SET_DUMPABLE);
+			VERIFY(sc.bool_setting.has_value());
+			VERIFY(sc.bool_setting->value() == true);
+			VERIFY(sc.res.has_value());
+			VERIFY(!sc.bool_res);
+		}), EXIT_VERIFY_CB(PrCtlSystemCall, {
+			VERIFY(sc.hasResultValue());
+		}), IgnoreCalls{0}, {
+			I386_CROSS_ABI(IgnoreCalls{0}, []() {
+				syscall32(SyscallNr32::PRCTL, PR_SET_DUMPABLE, 1);
+			})
+		}, "SET_DUMPABLE"
+	}, TestSpec{SystemCallNr::PRCTL, []() {
+			prctl(PR_SET_KEEPCAPS, 1);
+			prctl(PR_GET_KEEPCAPS);
+		}, ENTRY_VERIFY_CB(PrCtlSystemCall, {
+			VERIFY(sc.op.operation() ==
+					clues::item::ProcessOp::GET_KEEPCAPS);
+			VERIFY(!sc.bool_setting);
+			VERIFY(!sc.res);
+			VERIFY(sc.bool_res.has_value());
+		}), EXIT_VERIFY_CB(PrCtlSystemCall, {
+			VERIFY(sc.hasResultValue());
+			VERIFY(sc.bool_res->value() == true);
+		}), IgnoreCalls{1}, {
+			I386_CROSS_ABI(IgnoreCalls{1}, []() {
+				prctl(PR_SET_KEEPCAPS, 1);
+				syscall32(SyscallNr32::PRCTL, PR_GET_KEEPCAPS);
+			})
+		}, "GET_KEEPCAPS"
+	}, TestSpec{SystemCallNr::PRCTL, []() {
+			prctl(PR_SET_KEEPCAPS, 1);
+		}, ENTRY_VERIFY_CB(PrCtlSystemCall, {
+			VERIFY(sc.op.operation() ==
+					clues::item::ProcessOp::SET_KEEPCAPS);
+			VERIFY(sc.bool_setting.has_value());
+			VERIFY(sc.bool_setting->value() == true);
+			VERIFY(sc.res.has_value());
+			VERIFY(!sc.bool_res);
+		}), EXIT_VERIFY_CB(PrCtlSystemCall, {
+			VERIFY(sc.hasResultValue());
+		}), IgnoreCalls{0}, {
+			I386_CROSS_ABI(IgnoreCalls{0}, []() {
+				syscall32(SyscallNr32::PRCTL, PR_SET_KEEPCAPS, 1);
+			})
+		}, "SET_KEEPCAPS"
+	}, TestSpec{SystemCallNr::PRCTL, []() {
+			prctl(PR_SET_CHILD_SUBREAPER, 1);
+		}, ENTRY_VERIFY_CB(PrCtlSystemCall, {
+			VERIFY(sc.op.operation() ==
+					clues::item::ProcessOp::SET_CHILD_SUBREAPER);
+			VERIFY(sc.bool_setting.has_value());
+			VERIFY(sc.bool_setting->value() == true);
+			VERIFY(sc.res.has_value());
+			VERIFY(!sc.bool_res);
+		}), EXIT_VERIFY_CB(PrCtlSystemCall, {
+			VERIFY(sc.hasResultValue());
+		}), IgnoreCalls{0}, {
+			I386_CROSS_ABI(IgnoreCalls{0}, []() {
+				syscall32(SyscallNr32::PRCTL, PR_SET_CHILD_SUBREAPER, 1);
+			})
+		}, "SET_CHILD_SUBREAPER"
+	}, TestSpec{SystemCallNr::PRCTL, []() {
+			prctl(PR_GET_IO_FLUSHER);
+		}, ENTRY_VERIFY_CB(PrCtlSystemCall, {
+			VERIFY(sc.op.operation() ==
+					clues::item::ProcessOp::GET_IO_FLUSHER);
+			VERIFY(!sc.bool_setting);
+			VERIFY(!sc.res);
+			VERIFY(sc.bool_res.has_value());
+		}), EXIT_VERIFY_CB(PrCtlSystemCall, {
+			/* this call requires special permissions */
+			(void)sc;
+		}), IgnoreCalls{0}, {
+			I386_CROSS_ABI(IgnoreCalls{0}, []() {
+				syscall32(SyscallNr32::PRCTL, PR_GET_IO_FLUSHER);
+			})
+		}, "GET_IO_FLUSHER"
+	}, TestSpec{SystemCallNr::PRCTL, []() {
+			prctl(PR_SET_IO_FLUSHER, 1);
+		}, ENTRY_VERIFY_CB(PrCtlSystemCall, {
+			VERIFY(sc.op.operation() ==
+					clues::item::ProcessOp::SET_IO_FLUSHER);
+			VERIFY(sc.bool_setting.has_value());
+			VERIFY(sc.bool_setting->value() == true);
+			VERIFY(sc.res.has_value());
+			VERIFY(!sc.bool_res);
+		}), EXIT_VERIFY_CB(PrCtlSystemCall, {
+			(void)sc;
+			/* this call requires special permissions */
+		}), IgnoreCalls{0}, {
+			I386_CROSS_ABI(IgnoreCalls{0}, []() {
+				syscall32(SyscallNr32::PRCTL, PR_SET_IO_FLUSHER, 1);
+			})
+		}, "SET_IO_FLUSHER"
 #ifdef CLUES_HAVE_PIPE1
 	}, TestSpec{SystemCallNr::PIPE, []() {
 			int pipes[2];
